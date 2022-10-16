3d6c1aa87246e144bbd08a93ad73b98d3964a440
ClCage4 <112894479+ClCage4@users.noreply.github.com>
2022-10-16 10:56:01 -0500

    updated JS added Elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
+function getForecast(coordinates) {
+  let apiKey = "c563b906050778a90869f572a5baf264";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Memphis&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  //let cityElement = documenent.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
+  farenheitTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  //cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
+}
+function search(city) {
+  let apiKey = "c563b906050778a90869f572a5baf264";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function logSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", logSubmit);
+
+search("Memphis");
