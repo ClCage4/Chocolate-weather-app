2c885c6528ae3d64d57b7e9425edddce3c470477
ClCage4 <112894479+ClCage4@users.noreply.github.com>
2022-10-18 08:21:56 -0500

    displayedCelsiusTemperature

--- src/index.js
+++ src/index.js
@@ -67,6 +67,11 @@ function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
 
+  function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("temperature");
+  }
+
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
